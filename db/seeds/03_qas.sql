15543c23ad19e3baacbbb16d303da1903ef02f8e
Gleb Shkut <gleb.shkut@gmail.com>
2022-02-12 13:21:40 -0800

    add data to database

---
+++ db/seeds/03_qas.sql
@@ -0,0 +1,21 @@
+-- Users table seeds here (Example)
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (1, 'When Bologna massacre in Italy happened?', 'August 2, 1980', 'June 20, 1981', 'April 21, 2000', 'August 15, 1977');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (1, 'El Mozote massacre in El Salvador', 'December 11, 1981', 'June 18, 1981', 'December 21, 1980', 'May 3, 1977');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (1, 'The Rome and Vienna airport attacks', 'December 27, 1985', 'April 26, 1986', 'May 21, 1984', 'October 2, 1988');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (2, '2 + 2 * 2', '6', '8', '-4', '1');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (2, 'sin of 30 degrees', '1/2', '0.45', '1/3', '0.8');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (2, '84 * 102 = ?', '8568', '8234', '9024', '7992');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (2, 'What subject is it?', 'Math', 'Physics', 'Geography', 'I dont care');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (3, 'Do u like physics?', 'SURE!!', 'Not really', 'I wanna go home and play fortnite', 'Who r u?');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (3, 'Avagadros Law - year', '1811', '1822', 'Who cares...', '1821');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (3, 'Coulombs Law - year', '(1738-1806)', '(1740-1816)', 'What the second year means?', '(1748-1793)');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (4, 'Data types of Python', 'int, float, strings', 'INTEGER, DOUBLE, STRING', 'u can just use let', 'int, double, strings');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (4, 'Compound data structures of Python', 'lists, tuples, and dictionaries', 'lists, arrays, functions', 'just repositories', 'arrays, tuples, and dictionaries');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (4, 'Is there Object-oriented programming in Python', 'Yes', 'idk', 'No', 'Kind of');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (5, 'Why do we use DataBases?', 'In order to data stay safe after server reboot', 'Because we should use SQL queries', 'Its more difficult', 'Its slower');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (5, 'How to select all of the columns from table?', 'Using *', 'Using SELECT ALL', 'Type in every column only', 'Using SELECT .');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (5, 'If you need to select all of the group that have COUNT of more than 10, what you gonna use?', 'HAVING COUNT(*) > 10', 'WHERE COUNT(*) > 10', 'DROP TABLE', 'SELECT COUNT(*) > 10');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (6, 'Why do you have to use functions?', 'To make code DRY', 'To make code WET', 'To repeat the same code', 'To make my team cry');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (6, 'What is a promise is JS?', 'object represents the eventual completion of an asynchronous operation', 'This is when I asked my friend for money and didnt return it back', 'This is when I asked my friend for money and DID return it back', 'object represents the eventual completion of synchronous operation');
+INSERT INTO qas (quiz_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (6, 'Can you use JS outside of Browser?', 'Yes', 'Nope', 'If Project Manager will give a permission', 'Only in emergency cases');
+
