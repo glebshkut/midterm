de048f4596eea4bc03cb13bedd54c38502668a0a
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-02-14 14:01:51 -0800

    add drop down when create quiz has been clicked

--- public/scripts/dropdown-menu.js
+++ public/scripts/dropdown-menu.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
   $('.join-quiz-button').click(function (event) {
     event.preventDefault();
-    console.log("Is this working?");
+    renderQuizElements();
   });
 });
+
+
+const renderQuizElements = function() {
+  console.log("adding quiz to html");
+  // *** NEED TO ADD A FOR LOOP TO LOAD EACH QUIZ AVAILABLE (right now just loads one)***
+  let $quiz = createQuizElement();
+  $('.posted').prepend($quiz);
+};
+
+const createQuizElement = function() {
+  console.log("creating quiz element");
+  const $quiz = $(`
+    <div class="quiz">
+      <a>link to quiz</a>
+    </div>
+  `);
+
+  return $quiz;
+};
