ffb0ba9fe9906dbca4568029cb377f0d6aedace6
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-02-14 15:28:11 -0800

    add dropdown for creating a new quiz

--- public/scripts/dropdown-menu.js
+++ public/scripts/dropdown-menu.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     event.preventDefault();
     renderQuizElements();
   });
+
+  $('.new-quiz-button').click(function (event) {
+    event.preventDefault();
+    renderQuizEntry();
+  });
 });
 
 
@@ -11,6 +16,19 @@ const renderQuizElements = function() {
   // *** NEED TO ADD A FOR LOOP TO LOAD EACH QUIZ AVAILABLE (right now just loads one)***
   let $quiz = createQuizElement();
   $('.posted').prepend($quiz);
+  $('.posted').slideDown(400);
+};
+
+const renderQuizEntry = function () {
+  console.log($('.create-quiz').length);
+  if ($('.create-quiz').length === 1) {
+    let $quizEntry = creatQuizEntry();
+    $('.create-quiz').prepend($quizEntry);
+    $('.create-quiz').slideDown(400);
+  } else {
+    $('.create-quiz').slideUp(400);
+  }
+
 };
 
 const createQuizElement = function() {
@@ -22,4 +40,16 @@ const createQuizElement = function() {
   `);
 
   return $quiz;
+};
+
+const creatQuizEntry = function () {
+  const $entry = $(`
+    <form class="entry" METHOD="GET">
+        <h6>Name</h6>
+        <input placeholder="Enter quiz name here">
+        <h6>Subject</h6>
+        <input placeholder="enter quiz subject here">
+    <form>
+  `);
+  return $entry;
 };
