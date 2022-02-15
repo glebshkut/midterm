2ce6b12d31151b5f2f20d7a45419256f3fe8f69a
Melissa <mawilliamson10@gmail.com>
2022-02-15 05:47:38 +0000

    schema and seed files for attempts

---
+++ db/seeds/04_attempts.sql
@@ -0,0 +1,14 @@
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 1, 1);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 2, 3);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 3, 4);
+
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (2, 1, 1);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (2, 2, 1);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (2, 3, 1);
+
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (3, 1, 4);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (3, 2, 3);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (3, 3, 1);
+
+
+
--- routes/results.js
+++ routes/results.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+// module.exports = (db) => {
+//   router.get("", (req, res) => {
+//     res.render("../views/results");
+//   });
+//   return router;
+// };
+
 module.exports = (db) => {
-  router.get("", (req, res) => {
+  router.get("/:id/:quizID", (req, res) => {
+    const id = req.params.id;
+    const quizID = req.params.quizID;
+    db.query(`
+  SELECT count(given_answer)as correct
+FROM attempts
+JOIN qas ON attempts.qa_id = qas.id
+WHERE attempts.user_id = $1 AND qas.quiz_id = $2
+GROUP BY attempts.given_answer
+HAVING attempts.given_answer = 1;
+  `, [id, quizID])
+      .then(result => result.rows)
+      .catch(err => null);
     res.render("../views/results");
   });
-
   return router;
 };
