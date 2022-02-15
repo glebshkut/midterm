4cd99edddd3c18941471a0783a6f0b14b0dcc577
Melissa <mawilliamson10@gmail.com>
2022-02-15 21:52:36 +0000

    pull data from attempts database

--- routes/results.js
+++ routes/results.js
@@ -10,22 +10,27 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/quizID", (req, res) => {
-    const id = 1;
-    const quizID = 2;
+    let templateVars = {};
+    const id = 2;
+    const quizID = 1;
     console.log("HELLOOO");
     db.query(`
   SELECT count(given_answer)as correct
-FROM attempts;
-
-  `)
+FROM attempts
+JOIN qas ON attempts.qa_id = qas.id
+WHERE attempts.user_id = $1 AND qas.quiz_id = $2
+GROUP BY attempts.given_answer
+HAVING attempts.given_answer = 1;
+  `, [id, quizID])
       .then(result => {
-        const score = result.rows;
+        const score = result.rows[0];
+        templateVars = { score };
         console.log(score);
-        res.send({ result })
-
+        // res.send({ score })
+        res.render("results", templateVars);
       })
       .catch(err => console.log("error", err));
-    res.render("../views/results");
+
   });
   return router;
 };
