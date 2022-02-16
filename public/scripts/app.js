410f8a6f6f38cb4ac7844fe48bd44098d6aaf728
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-02-16 15:44:21 -0800

    clear branch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,43 +27,46 @@ $(document).ready(function() {
 
   $('body').on("click",'.submit-quiz',function(event) {
     event.preventDefault();
-    console.log("sending ajax post to /quiz/add");
-    $.ajax({
-      url : '/quiz/add',
-      type : 'POST',
-      dataType:'json',
-      data: {quizName,quizSubject},
-      success : function(data) {
-        console.log(JSON.stringify(data));
-        let id = JSON.stringify(data);
-        id = JSON.parse(id);
-        id = id.id[0].id;
 
-        while (numOfQuestions > 0) {
-          if (sessionStorage[`Q${numOfQuestions}`]) {
-            let questionObj = sessionStorage[`Q${numOfQuestions}`];
-            questionObj = JSON.parse(questionObj);
-            let question = questionObj.question;
-            let correct = questionObj.correct;
-            let wrong1 = questionObj.wrong1;
-            let wrong2 = questionObj.wrong2;
-            let wrong3 = questionObj.wrong3;
-            console.log("sending ajax post to /quiz/addQuestion");
-            $.ajax({
-              url : '/quiz/addQuestion',
-              type : 'POST',
-              dataType : 'json',
-              data : {id, question, correct, wrong1, wrong2, wrong3}
-            });
+    if (numOfQuestions > 0) {
+      console.log("sending ajax post to /quiz/add");
+      $.ajax({
+        url : '/quiz/add',
+        type : 'POST',
+        dataType:'json',
+        data: {quizName,quizSubject},
+        success : function(data) {
+          console.log(JSON.stringify(data));
+          let id = JSON.stringify(data);
+          id = JSON.parse(id);
+          id = id.id[0].id;
+
+          while (numOfQuestions > 0) {
+            if (sessionStorage[`Q${numOfQuestions}`]) {
+              let questionObj = sessionStorage[`Q${numOfQuestions}`];
+              questionObj = JSON.parse(questionObj);
+              let question = questionObj.question;
+              let correct = questionObj.correct;
+              let wrong1 = questionObj.wrong1;
+              let wrong2 = questionObj.wrong2;
+              let wrong3 = questionObj.wrong3;
+              console.log("sending ajax post to /quiz/addQuestion");
+              $.ajax({
+                url : '/quiz/addQuestion',
+                type : 'POST',
+                dataType : 'json',
+                data : {id, question, correct, wrong1, wrong2, wrong3}
+              });
+            }
+            numOfQuestions -= 1;
           }
-          numOfQuestions -= 1;
-        }
 
-      },
-      error : function(request,error) {
-        console.log("Request: " + JSON.stringify(request));
-      }
-    });
+        },
+        error : function(request,error) {
+          console.log("Request: " + JSON.stringify(request));
+        }
+      });
+    }
   });
 });
 
