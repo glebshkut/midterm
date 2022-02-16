b1fb967358303ce22b15f306911db8d89ab73d6c
Melissa <mawilliamson10@gmail.com>
2022-02-16 02:36:05 +0000

    alter attemps seeds table

--- db/seeds/04_attempts.sql
+++ db/seeds/04_attempts.sql
@@ -1,4 +1,4 @@
-INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 1, 1);
+INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 1, 2);
 INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 2, 3);
 INSERT INTO attempts(user_id, qa_id, given_answer) VALUES (1, 3, 4);
 
